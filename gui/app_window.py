5c098ced6de03a15e46813d09ecbd98f4c6fbffa
Mattia Di Profio <mattia.diprofio@gmail.com>
2023-11-12 18:21:29 +0000

    refactor gui folder

--- gui/app_window.py
+++ gui/app_window.py
@@ -1,8 +1,10 @@
+# Import built-in modules
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
-
 import sys
+
+# Import required classes
 sys.path.append("../")
 from objects.student import Student
 from objects.internship import Internship
@@ -13,8 +15,10 @@ from business.mysql_workbench import MySQLWorkbenchInterface
 from .settings_window import Settings
 
 class AppWindow(Tk):
+
     def __init__(self):
         super().__init__()
+        # Set up interfaces defined in 'business' folder
         self.workbench = MySQLWorkbenchInterface()
         self.matcher = Matcher()
             
@@ -22,7 +26,7 @@ class AppWindow(Tk):
         self.workbench.destroy_db("alpha_db")
         self.workbench.create_db("alpha_db")
 
-        # Set up populator and database
+        # Set up populator and database interfaces
         self.populator = DatabasePopulator()
         self.db = DatabaseInterface("alpha_db")
 
@@ -35,29 +39,83 @@ class AppWindow(Tk):
         internships_input = 'C:\\Users\\matti\\OneDrive\\Desktop\\CS3028 Project\\mysql-interface\\inputs\\internshipsdata.csv'
         self.populator.populate_via_csv(students_input, internships_input)
 
+        # Configure window appearance
         self.title("CS3028 Team Alpha")
         self.geometry("500x350")
         self.resizable(False, False)
         self.configure(bg='#003f88')
 
-        # frame used to position widgets on this window
+        # Frame used to position widgets on this window
         self.buttons_frame = Frame(self)
         self.buttons_frame.configure(bg='#003f88')
 
-        # variable deciding whether output printed to console
+        # Variable to determine whether output printed to console
         self.print_output = IntVar()
 
-        Label(self, text = 'Internship Matchmaker', font = ('Calibri', 15, 'bold'), fg='#ffffff', bg='#003f88').pack(pady=15)
-        Label(self, text = "Run matcher, or configure custom matcher settings.\n Results will be automatically stored in 'matches.csv'", bg='#003f88', fg='#ffffff', font = ('Calibri', 12)).pack(pady=10, padx=15)
-        Button(self.buttons_frame, text = 'Run Match', font = ('Calibri', 12, 'bold'), width=12, fg='#003f88', activeforeground='#003f88', activebackground='#f1a13b', bg='#fdc500', relief=FLAT, command = self.run_default_matcher).pack(fill= BOTH, expand= True, pady= 10)
-        Button(self.buttons_frame, text = 'Settings', font = ('Calibri', 12, 'bold'), width=12, fg='#003f88', activeforeground='#003f88', activebackground='#f1a13b', bg='#fdc500', relief=FLAT, command = self.configure_settings).pack(fill= BOTH, expand= True, pady= 10)
+        # Instruction for use labels
+        Label(
+            self, 
+            text = 'Internship Matchmaker', 
+            font = ('Calibri', 15, 'bold'), 
+            fg='#ffffff', 
+            bg='#003f88'
+        ).pack(pady=15)
+
+        Label(
+            self, 
+            text = "Run matcher, or configure custom matcher settings.\n Results will be automatically stored in 'matches.csv'", 
+            bg='#003f88', 
+            fg='#ffffff', 
+            font = ('Calibri', 12)
+        ).pack(pady=10, padx=15)
+
+        # Button to run default matching algorithm
+        Button(
+            self.buttons_frame, 
+            text = 'Run Match', 
+            font = ('Calibri', 12, 'bold'), 
+            width=12, 
+            fg='#003f88', 
+            activeforeground='#003f88', 
+            activebackground='#f1a13b', 
+            bg='#fdc500', 
+            relief=FLAT, 
+            command = self.run_default_matcher
+        ).pack(fill= BOTH, expand= True, pady= 10)
+
+        # Button to configure custom settings for matching algorithm
+        Button(
+            self.buttons_frame, 
+            text = 'Settings', 
+            font = ('Calibri', 12, 'bold'), 
+            width=12, 
+            fg='#003f88', 
+            activeforeground='#003f88', 
+            activebackground='#f1a13b', 
+            bg='#fdc500', 
+            relief=FLAT, 
+            command = self.configure_settings
+        ).pack(fill= BOTH, expand= True, pady= 10)
         
-        # add check button to print to terminal
-        Checkbutton(self.buttons_frame, text="Print results to console", variable = self.print_output, onvalue = 1, offvalue = 0, bg='#003f88', activebackground='#003f88', activeforeground='#ffffff', selectcolor='#003f88' , fg='#ffffff', font = ('Calibri', 12)).pack(anchor=W, pady=10)
+        # Checkbutton to display matching results to terminal in addition to writing them to 'matches.csv'
+        Checkbutton(
+            self.buttons_frame, 
+            text="Print results to console", 
+            variable = self.print_output, 
+            onvalue = 1, 
+            offvalue = 0, 
+            bg='#003f88', 
+            activebackground='#003f88', 
+            activeforeground='#ffffff', 
+            selectcolor='#003f88' , 
+            fg='#ffffff', 
+            font = ('Calibri', 12)
+        ).pack(anchor=W, pady=10)
 
         self.buttons_frame.pack()
         self.mainloop()
 
+    # Run the default matching algorithm, where all fields have equal priority
     def run_default_matcher(self):
         # Fetch students and internships from the database
         student_records = self.db.get_table("students")
@@ -70,22 +128,21 @@ class AppWindow(Tk):
         # Match students with internships and save matches to csv
         matches, unmatched = self.matcher.filter_matches(students, internships)
 
+        # Print results of matching algorithm to terminal, if checkbox was ticked
         if self.print_output.get() == 1:
             print("Matched students : ")
-            # Print stringified matches 
             for (student, internship) in matches.items():
                 print(f"{student.get_fullname()} matched for {internship.get_title()} at {internship.get_organization()}")
             print("\nUnmatched students : ")
-            # Print names of unmatched students
             for student in unmatched:
                 print(student.get_fullname())
 
+    # Invoke the 'settings' window
     def configure_settings(self):
         Settings(self.run_custom_matcher)
 
+    # Run custom matching algorithm by applying user settings
     def run_custom_matcher(self, settings):
         print("\nCustom settings to be applied : ")
         print(settings)
-
-        # Next : modify matcher so that if data parameter passed it is used in the algorithm
-        # Run matcher here
+        # TODO : implement custom matching algorithm
--- gui/settings_window.py
+++ gui/settings_window.py
@@ -1,97 +1,234 @@
+# Import built-in modules
 from tkinter import *
 from tkinter import messagebox
 from tkinter import ttk
 
 class Settings(Frame):
+
     def __init__(self, func):
         super().__init__()
-        self.func = func # allows data to be send to the window which invoked the current one
+        # Bridge function connected to main window
+        self.func = func
         self.top = Toplevel()
         self.frame = Frame(self.top)
         self.top.title("Matcher Settings")
         self.top.geometry("500x400")
 
-        # Settings to be applied 
-        self.degree = IntVar()
-        self.score = IntVar()
-        self.experience = IntVar()
-        self.study_mode = IntVar()
-        self.study_pattern = IntVar()
+        # Settings variables
+        [ self.degree, self.score, self.experience, self.study_mode, self.study_pattern ] = [ IntVar(), IntVar(), IntVar(), IntVar(), IntVar() ]
 
-        Label(self.top, text = "Configure matcher settings", bg='#003f88', fg='#ffffff', font = ('Calibri', 12, 'bold')).pack(pady=10)
+        Label(
+            self.top, 
+            text = "Configure matcher settings", 
+            bg='#003f88', fg='#ffffff', 
+            font = ('Calibri', 12, 'bold')
+        ).pack(pady=10)
 
-        # frame to position text widgets
+        # Frame used to position text widgets
         self.text_frame = Frame(self.top)
         self.text_frame.pack(pady=9)
         self.text_frame.configure(bg='#003f88')
         self.top.configure(bg='#003f88')
 
-        Label(self.text_frame, text = "• Select/deselect criterias and assign them a priority 1 (low) to 5 (high)",  bg='#003f88', fg='#ffffff', font = ('Calibri', 12)).pack(anchor=W)
-        Label(self.text_frame, text = "• Student score will be selected by default ", bg='#003f88', fg='#ffffff', font = ('Calibri', 12)).pack(anchor=W)
-        Label(self.text_frame, text = "• If not all fields ticked are assigned a priority they will default to 1", bg='#003f88', fg='#ffffff', font = ('Calibri', 12)).pack(anchor=W)
+        # Instructions label
+        Label(
+            self.text_frame, 
+            text = "• Select/deselect criterias and assign them a priority 1 (low) to 5 (high)",  
+            bg='#003f88', 
+            fg='#ffffff', 
+            font = ('Calibri', 12)
+        ).pack(anchor=W)
 
-        # frame to position settings picker
+        # Define parent frame to position this window's widgets
         self.settings_frame = Frame(self.top)
         self.settings_frame.pack(pady=10)
         self.settings_frame.configure(bg='#003f88')
         self.top.configure(bg='#003f88')
 
-        # settings pickers 
+        # -------------------------------------- DEGREE ------------------------------------------
 
-        # 1. Degree checkbox and priority picker
+        # Define frame to hold the checkbox and priority dropdown for the 'degree' field
         self.degree_checkbox_frame = Frame(self.settings_frame)
         self.degree_checkbox_frame.configure(bg='#003f88')
-        self.degree_checkbox_frame.columnconfigure(0, weight=1)
 
-        Checkbutton(self.degree_checkbox_frame, text="Degree", variable = self.degree, command = self.toggle_priority_dropdown, onvalue = 1, offvalue = 0, bg='#003f88', activebackground='#003f88', activeforeground='#ffffff', selectcolor='#003f88' , fg='#ffffff', font = ('Calibri', 12)).grid(row=0, column=0)
-        self.degree_priority_combo = ttk.Combobox(self.degree_checkbox_frame, value = ["1", "2", "3", "4", "5"], width=2, state='disabled')
+        # Configure checkbutton for the 'degree' field
+        Checkbutton(
+            self.degree_checkbox_frame, 
+            text="Degree", 
+            variable = self.degree, 
+            command = self.toggle_priority_dropdown, 
+            onvalue = 1, 
+            offvalue = 0, 
+            bg='#003f88', 
+            activebackground='#003f88', 
+            activeforeground='#ffffff', 
+            selectcolor='#003f88' , 
+            fg='#ffffff', 
+            font = ('Calibri', 12)
+        ).grid(row=0, column=0)
+
+        # Configure priority picker for the 'degree' field
+        self.degree_priority_combo = ttk.Combobox(
+            self.degree_checkbox_frame, 
+            value = ["1", "2", "3", "4", "5"], 
+            width=2, 
+            state='disabled'
+        )
         self.degree_priority_combo.current(0)
         self.degree_priority_combo.grid(row=0, column=1)
         self.degree_checkbox_frame.pack(anchor=W)
         
-        # 2. Score checkbox and priority picker
+        # -------------------------------------- SCORE ------------------------------------------
+
+        # Define frame to hold the checkbox and priority dropdown for the 'score' field
         self.score_checkbox_frame = Frame(self.settings_frame)
         self.score_checkbox_frame.configure(bg='#003f88')
-        Checkbutton(self.score_checkbox_frame, text="Score", variable = self.score, command = self.toggle_priority_dropdown, onvalue = 1, offvalue = 0, bg='#003f88', activebackground='#003f88', activeforeground='#ffffff', selectcolor='#003f88' , fg='#ffffff', font = ('Calibri', 12)).grid(row=1, column=0)
-        self.score_priority_combo = ttk.Combobox(self.score_checkbox_frame, value = ["1", "2", "3", "4", "5"], width=2, state='disabled')
+
+        # Configure checkbutton for the 'score' field
+        Checkbutton(
+            self.score_checkbox_frame, 
+            text="Score", 
+            variable = self.score, 
+            command = self.toggle_priority_dropdown, 
+            onvalue = 1, 
+            offvalue = 0, 
+            bg='#003f88', 
+            activebackground='#003f88', 
+            activeforeground='#ffffff', 
+            selectcolor='#003f88' , 
+            fg='#ffffff', 
+            font = ('Calibri', 12)
+        ).grid(row=1, column=0)
+        
+        # Configure priority picker for the 'score' field
+        self.score_priority_combo = ttk.Combobox(
+            self.score_checkbox_frame, 
+            value = ["1", "2", "3", "4", "5"], 
+            width=2, 
+            state='disabled'
+        )
         self.score_priority_combo.current(0)
         self.score_priority_combo.grid(row=1, column=1)
         self.score_checkbox_frame.pack(anchor=W)
 
-        # 3. Experience checkbox and priority picker
+        # -------------------------------------- EXPERIENCE ------------------------------------------
+
+        # Define frame to hold the checkbox and priority dropdown for the 'experience' field
         self.experience_checkbox_frame = Frame(self.settings_frame)
-        Checkbutton(self.experience_checkbox_frame, text="Experience", variable = self.experience, command = self.toggle_priority_dropdown, onvalue = 1, offvalue = 0, bg='#003f88', activebackground='#003f88', activeforeground='#ffffff', selectcolor='#003f88' , fg='#ffffff', font = ('Calibri', 12)).grid(row=2, column=0)
-        self.experience_priority_combo = ttk.Combobox(self.experience_checkbox_frame, value = ["1", "2", "3", "4", "5"], width=2, state='disabled')
+        self.experience_checkbox_frame.configure(bg='#003f88')
+
+        # Configure checkbutton for the 'experience' field
+        Checkbutton(
+            self.experience_checkbox_frame, 
+            text="Experience", 
+            variable = self.experience, 
+            command = self.toggle_priority_dropdown, 
+            onvalue = 1, 
+            offvalue = 0, 
+            bg='#003f88', 
+            activebackground='#003f88', 
+            activeforeground='#ffffff', 
+            selectcolor='#003f88' , 
+            fg='#ffffff', 
+            font = ('Calibri', 12)
+        ).grid(row=2, column=0)
+        
+        # Configure priority picker for the 'experience' field
+        self.experience_priority_combo = ttk.Combobox(
+            self.experience_checkbox_frame, 
+            value = ["1", "2", "3", "4", "5"], 
+            width=2, 
+            state='disabled'
+        )
         self.experience_priority_combo.current(0)
         self.experience_priority_combo.grid(row=2, column=1)
-        self.experience_checkbox_frame.configure(bg='#003f88')
         self.experience_checkbox_frame.pack(anchor=W)
 
-        # 4. Study mode checkbox and priority picker
+        # -------------------------------------- STUDY MODE ------------------------------------------
+
+        # Define frame to hold the checkbox and priority dropdown for the 'study mode' field
         self.studymode_checkbox_frame = Frame(self.settings_frame)
-        Checkbutton(self.studymode_checkbox_frame, text="Study mode", variable = self.study_mode, command = self.toggle_priority_dropdown, onvalue = 1, offvalue = 0, bg='#003f88', activebackground='#003f88', activeforeground='#ffffff', selectcolor='#003f88' , fg='#ffffff', font = ('Calibri', 12)).grid(row=3, column=0)
-        self.studymode_priority_combo = ttk.Combobox(self.studymode_checkbox_frame, value = ["1", "2", "3", "4", "5"], width=2, state='disabled')
+        self.studymode_checkbox_frame.configure(bg='#003f88')
+
+        # Configure checkbutton for the 'study mode' field
+        Checkbutton(
+            self.studymode_checkbox_frame, 
+            text="Study mode", 
+            variable = self.study_mode, 
+            command = self.toggle_priority_dropdown, 
+            onvalue = 1, 
+            offvalue = 0,
+            bg='#003f88', 
+            activebackground='#003f88', 
+            activeforeground='#ffffff', 
+            selectcolor='#003f88' , 
+            fg='#ffffff', 
+            font = ('Calibri', 12)
+        ).grid(row=3, column=0)
+        
+        # Configure priority picker for the 'study mode' field
+        self.studymode_priority_combo = ttk.Combobox(
+            self.studymode_checkbox_frame, 
+            value = ["1", "2", "3", "4", "5"], 
+            width=2, 
+            state='disabled'
+        )
         self.studymode_priority_combo.current(0)
         self.studymode_priority_combo.grid(row=3, column=1)
-        self.studymode_checkbox_frame.configure(bg='#003f88')
         self.studymode_checkbox_frame.pack(anchor=W)
 
-        # 5. Study pattern checkbox and priority picker
+        # -------------------------------------- STUDY PATTERN ------------------------------------------
+
+        # Define frame to hold the checkbox and priority dropdown for the 'study pattern' field
         self.studypattern_checkbox_frame = Frame(self.settings_frame)
-        Checkbutton(self.studypattern_checkbox_frame, text="Study pattern", variable = self.study_pattern, command = self.toggle_priority_dropdown, onvalue = 1, offvalue = 0, bg='#003f88', activebackground='#003f88', activeforeground='#ffffff', selectcolor='#003f88' , fg='#ffffff', font = ('Calibri', 12)).grid(row=4, column=0)      
-        self.studypattern_priority_combo = ttk.Combobox(self.studypattern_checkbox_frame, value = ["1", "2", "3", "4", "5"], width=2, state='disabled')
-        self.studypattern_priority_combo.current(0)
-        self.studypattern_priority_combo.grid(row=4, column=1)
         self.studypattern_checkbox_frame.configure(bg='#003f88')
+
+        # Configure checkbutton for the 'study mode' field
+        Checkbutton(
+            self.studypattern_checkbox_frame, 
+            text="Study pattern", 
+            variable = self.study_pattern, 
+            command = self.toggle_priority_dropdown, 
+            onvalue = 1, 
+            offvalue = 0,
+            bg='#003f88', 
+            activebackground='#003f88', 
+            activeforeground='#ffffff', 
+            selectcolor='#003f88' , 
+            fg='#ffffff', 
+            font = ('Calibri', 12)
+        ).grid(row=3, column=0)
+        
+        # Configure priority picker for the 'study mode' field
+        self.studypattern_priority_combo = ttk.Combobox(
+            self.studypattern_checkbox_frame, 
+            value = ["1", "2", "3", "4", "5"], 
+            width=2, 
+            state='disabled'
+        )
+        self.studypattern_priority_combo.current(0)
+        self.studypattern_priority_combo.grid(row=3, column=1)
         self.studypattern_checkbox_frame.pack(anchor=W)
 
-        # Button to send settings packet back to main window
-        Button(self.settings_frame, text = 'Apply & Match', font = ('Calibri', 12, 'bold'), width=15, fg='#003f88', activeforeground='#003f88', activebackground='#f1a13b', bg='#fdc500', relief=FLAT, command = self.send_data).pack(anchor=W, pady=15)
+        # Button responsible for sending 'settings' object back to main window
+        Button(self.settings_frame, 
+            text = 'Apply & Match', 
+            font = ('Calibri', 12, 'bold'), 
+            width=15, 
+            fg='#003f88', 
+            activeforeground='#003f88', 
+            activebackground='#f1a13b', 
+            bg='#fdc500', 
+            relief=FLAT, 
+            command = self.send_data
+        ).pack(anchor=W, pady=15)
 
-    # transfer settings back to the parent window
+    # -------------------------------------- AUXILIARY FUNCTIONS ------------------------------------------
+
+    # Transfer settings back to the parent window
     def send_data(self):
 
-        # store settings
+        # Collect settings values at timestamp where send_date was invoked
         settings = {
             'origin' : 'settings',
             'degree' : {
@@ -116,18 +253,14 @@ class Settings(Frame):
             }
         }
 
+        # Invoke the bridge function and destroy the settings window
         self.func(settings)
         self.top.destroy()
 
-    #for each dropdown, allow selection only if corresponding checkbox is selected
+    # For each dropdown, allow selection only if corresponding checkbox is selected
     def toggle_priority_dropdown(self):
-        # Degree 
         self.degree_priority_combo.config(state = 'normal' if self.degree.get() == 1 else 'disabled')
-        # Score
         self.score_priority_combo.config(state = 'normal' if self.score.get() == 1 else 'disabled')
-        # Experience
         self.experience_priority_combo.config(state = 'normal' if self.experience.get() == 1 else 'disabled')
-        # Study mode
         self.studymode_priority_combo.config(state = 'normal' if self.study_mode.get() == 1 else 'disabled')
-        # Study pattern
         self.studypattern_priority_combo.config(state = 'normal' if self.study_pattern.get() == 1 else 'disabled')
