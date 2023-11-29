d50aeef8f5a8165dfcd2cf1e89a452b849f1aefb
Mattia Di Profio <mattia.diprofio@gmail.com>
2023-11-29 14:58:54 +0000

    final commit

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # Database APIs credentials
-DBUSERNAME='root'
-DBPASSWORD='TeamAlpha123!'
+DBUSERNAME='your_username'
+DBPASSWORD='your_password'
 
 # Input file absolute paths
-STUDENTS_DATA_INPUT='C:\\Users\matti\\OneDrive\\Desktop\\CS3028\\mysql-interface\\inputs\\studentsdata.csv'
-INTERNSHIP_DATA_INPUT='C:\\Users\\matti\\OneDrive\\Desktop\\CS3028\\mysql-interface\\inputs\\internshipsdata.csv'
+STUDENTS_DATA_INPUT='students_csv_input_path'
+INTERNSHIP_DATA_INPUT='internships_csv_input_path'
 
 # Output files absolute paths
-STUDENTS_DATA_OUTPUT='C:\\Users\matti\\OneDrive\\Desktop\\CS3028\\mysql-interface\\output\\studentsdata.csv'
-INTERNSHIP_DATA_OUTPUT='C:\\Users\matti\\OneDrive\\Desktop\\CS3028\\mysql-interface\\output\\internshipsdata.csv'
-MATCHES_OUTPUT='C:\\Users\matti\\OneDrive\\Desktop\\CS3028\\mysql-interface\\output\\matches.csv'
+STUDENTS_DATA_OUTPUT='students_csv_output_path'
+INTERNSHIP_DATA_OUTPUT='internships_csv_output_path'
+MATCHES_OUTPUT='matches_csv_output_path'
